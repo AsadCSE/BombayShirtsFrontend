402b108da45b6f02e6cf7a9ef95ff1f5c4f3593c
Asad Bin Abdullah <asadcse247@gmail.com>
2020-10-15 12:16:24 +0600

    init commit

---
+++ precache-manifest.ac9b0cd77bdbaca46e464cc565e83a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3225b4bb37e3fe82fc5a461c34dca4bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a0acfdfdff4bdab97f6",
+    "url": "/static/css/main.11f87492.chunk.css"
+  },
+  {
+    "revision": "ef3ca34251741f0f7049",
+    "url": "/static/js/2.4055c310.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4055c310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0acfdfdff4bdab97f6",
+    "url": "/static/js/main.025f74f0.chunk.js"
+  },
+  {
+    "revision": "2979dd4a6c801f2b096f",
+    "url": "/static/js/runtime-main.c5978360.js"
+  }
+]);
